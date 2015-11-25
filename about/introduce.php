3ba9ae5f9383cf6ae95508206258b2f617d940de
Kim Jaehong <jjaekong@gmail.com>
2015-11-25 12:09:56 +0900

    about > 회사소개: 작업 완료

---
+++ about/introduce.php
@@ -0,0 +1,51 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+    <title>WAWA VENTURES</title> 
+    <link href="/css/default.css" rel="stylesheet">
+    <link href="/css/about.css" rel="stylesheet">
+</head>
+<body class="about">
+    <a href="#content" class="skip sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+    <div id="wrapper">
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+        <main id="content" class="introduce">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="page-header">
+                            <h2>와와벤쳐스</h2>
+                            <p>
+                                창의적이고 혁신적인 상상을 통해 새로운 변화를 현실로 만들어가는 기업입니다.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="feature">
+                            <img class="img-responsive" src="/images/about/img_c.png">
+                            <h3>CREATIVE</h3>
+                            <p>와와벤처스는 창의와 혁신을 통해 변화를 만들어가는 사람들과 그들을 응원하는 사람들이 만나는 광장입니다.</p>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="feature">
+                            <img class="img-responsive" src="/images/about/img_r.png">
+                            <h3>REALITY</h3>
+                            <p>이 곳에서 여러분은 다양한 분야에서 불고있는 새로운 변화의 바람을 목격하고, 또한 그 변화를 만들어 가는 과정에 참여할 수 있습니다.</p>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="feature">
+                            <img class="img-responsive" src="/images/about/img_m.png">
+                            <h3>MARKER</h3>
+                            <p>창의적인 상상과 바램이 현실이 되어가는 놀라운 현장의 목격자로 여러분을 초대합니다. 감사합니다.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    </div>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docjs.php'); ?>
+    
+</body>
+</html>

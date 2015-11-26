b936abfbacf3696c1d68f0afbe41441b54dc9b2a
Kim Jaehong <jjaekong@gmail.com>
2015-11-27 00:09:01 +0900

    how > 시작하기: 작업 완료

--- works/start.php
+++ works/start.php
@@ -8,6 +8,41 @@
     <div id="wrapper">
         <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
         <main id="content" class="start">
+            <?php require($_SERVER['DOCUMENT_ROOT'].'/inc/path.php'); ?>
+            <div class="container">
+                <div class="page-header">
+                    <h2 class="page-title">프로젝트 신청 전 주의사항</h2>
+                </div>
+                <div class="guide">
+                    <p>
+                        와와벤처스는 아래와 같은 내용을 포함한 프로젝트에 대하여는 프로젝트 신청을 받지 않고 있으니 프로젝트 신청시 참고하시기 바랍니다.
+                    </p>
+                    <ol>
+                        <li>1.  금전 기타 유가증권(채무증권, 지분증권, 투자증권 등)의 제공을 약속하는 프로젝트</li>
+                        <li>2.  사행성, 음란성, 정치/이념적 편향성을 지닌 프로젝트</li>
+                        <li>3.  보상품의 제공 등 프로젝트에서 약속한 내용이 실현가능성이 현저히 낮다고 여겨지는 프로젝트</li>
+                        <li>4.  기타 사회상규 및 건전한 시민의식에 저해되는 내용의 프로젝트</li>
+                    </ol>
+                </div>
+                <section class="contact">
+                    <h3>연락처</h3>
+                    <dl>
+                        <dt><i class="icon-phone"></i> TEL</dt>
+                        <dd class="colon" role="separator">:</dd>
+                        <dd>02-593-2210</dd>
+                    </dl>
+                    <dl>
+                        <dt><i class="icon-mail"></i> E-mail</dt>
+                        <dd class="colon" role="separator">:</dd>
+                        <dd>gbcho@wawaventures.com</dd>
+                    </dl>
+                </section>
+                <div class="btn-area">
+                    <p>
+                        <a href="#" class="btn btn-primary btn-lg">프로젝트 신청하기</a>
+                    </p>
+                </div>
+            </div>
         </main>
         <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     </div>

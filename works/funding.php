0befd24df3ac8f72a692eee17c9cf961edfa8403
Kim Jaehong <jjaekong@gmail.com>
2015-11-26 13:03:01 +0900

    하우 > 크라우드펀딩이란: 작업 완료

---
+++ works/funding.php
@@ -0,0 +1,82 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+    <title>WAWA VENTURES</title> 
+    <link href="/css/default.css" rel="stylesheet">
+    <link href="/css/works.css" rel="stylesheet">
+</head>
+<body class="works">
+    <a href="#content" class="skip sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+    <div id="wrapper">
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+        <main id="content" class="funding">
+            <div class="container">
+                <section class="overview">
+                    <div class="section-header">
+                        <h2 class="section-title">크라우드 펀딩(Crowd Funding)이란?</h2>
+                        <p>
+                            크라우드 펀딩은 <i>"대중으로부터 자금을 모은다"</i>는 뜻으로<br>
+                            창의적인 아이디어에 기반한 새로운 제품과 컨텐츠 생산에 필요한 자금을 소셜네트워크나<br>
+                            인터넷 등의 매체를 통해서 대중으로부터 지원받은 방식을 의미합니다.
+                        </p>
+                    </div>
+                </section>
+                <section class="type">
+                    <div class="section-header">
+                        <h2 class="section-title">크라우드크라우드 펀딩의 유형</h2>
+                        <p>
+                            크라우드 펀딩은 일반적으로 다음과 같은 네 가지 유형으로 분류될 수 있습니다.<br>
+                            와와벤처스는 네가지 유형 중 후원형의 크라우드 펀딩을 진행하고 있으며, 향후 다른 유형의 펀딩으로 그 영역을 확대해 갈 예정입니다.
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-3">
+                            <div class="feature">
+                                <img class="img-responsive" src="/images/works/img_fundiding_type_1.gif">
+                                <h3>후원형</h3>
+                                <p>신기술이나 새로운 아이디어를 가진 스타트업 또는 개인이 새로운 제품 및 컨텐츠 생산을 위해 펀딩을 신청하고,펀딩을 한 대중에게 보상품을 제공하는 방식</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-3">
+                            <div class="feature">
+                                <img class="img-responsive" src="/images/works/img_fundiding_type_2.gif">
+                                <h3>기부형</h3>
+                                <p>공익적 활동 등을 위한 자금모집을 위해 보상을 바라지 않고 기부하는 방식</p>
+                            </div>
+                        </div>
+                        <div class="clearfix visible-xs-block"></div>
+                        <div class="col-xs-6 col-sm-3">
+                            <div class="feature">
+                                <img class="img-responsive" src="/images/works/img_fundiding_type_3.gif">
+                                <h3>대출형</h3>
+                                <p>스타트업이나 개인이 특정활동에 필요한 자금을 대출형태로 자금을 모집하고 그 대가로 이자를 지급하는 방식</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-3">
+                            <div class="feature">
+                                <img class="img-responsive" src="/images/works/img_fundiding_type_4.gif">
+                                <h3>투자형</h3>
+                                <p>후원형과 유사한 형태로 대중에게 보상품이 아닌 지분증권 등 수익을 배분하여 주는 방식</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="process">
+                    <div class="section-header">
+                        <h2 class="section-title">크라우드펀딩 진행 과정</h2>
+                        <p>크라우드 펀딩의 프로젝트 신청부터 제품 발송까지의 진행 과정을 알려드립니다.</p>
+                    </div>
+                    <div class="step">
+                        <p>
+                            <img class="img-responsive hidden-xs hidden-sm hidden-md" src="/images/works/img_steps_lg.gif">
+                            <img class="img-responsive hidden-lg" src="/images/works/img_steps_xs.gif">
+                        </p>
+                    </div>
+                </section>
+            </div>
+        </main>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    </div>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docjs.php'); ?>
+    
+</body>
+</html>
---
+++ works/start.php
@@ -0,0 +1,18 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+    <title>WAWA VENTURES</title> 
+    <link href="/css/default.css" rel="stylesheet">
+    <link href="/css/works.css" rel="stylesheet">
+</head>
+<body class="works">
+    <a href="#content" class="skip sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+    <div id="wrapper">
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+        <main id="content" class="start">
+        </main>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    </div>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docjs.php'); ?>
+    
+</body>
+</html>
